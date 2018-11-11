cf0e6ea110c0336d7fc616093848992197ef56aa
Mitch <kimbalm2@wwu.edu>
2018-11-11 15:11:14 -0800

    added client functionality except for timeouts.

--- prog2_client.c
+++ prog2_client.c
@@ -107,11 +107,73 @@ void play_game(int sd) {
 }
 */
 
+void turn_handler(int sd) {
+    char turnFlag;
+    ssize_t n;
+    uint8_t guessSize;
+    char guess[MAXWORDSIZE];
+    uint8_t isCorrect;
+
+    memset(guess,0,sizeof(guess));
+
+    n = recv(sd, turnFlag, sizeof(turnFlag), 0);
+    if (n != turnFlag) {
+        perror("Read Error: Turn flag not read properly");
+        exit(EXIT_FAILURE);
+    }
+
+    if(turnFlag == 'Y') {
+        //ap
+        printf("Your turn, enter a word: ");
+        //TODO read from thing
+        fgets(guess,MAXWORDSIZE,stdin);
+        guess[strlen(guess)-1]=0;
+
+        guessSize = (uint8_t)strlen(guess);
+        send(sd,&guessSize,sizeof(guessSize),0);
+        send(sd,guess,guessSize,0);
+        recv(sd,&isCorrect,sizeof(isCorrect),0);
+
+        if(isCorrect == 1){
+            printf("\nValid word!\n");
+        }
+        else{
+            printf("\nInvalid word!\n");
+        }
+
+    }else {
+        //iap
+        printf("Please wait for opponent to enter word... \n");
+
+        recv(sd,&isCorrect,sizeof(isCorrect),0);
+
+        if(isCorrect == 1){
+            n = recv(sd, &guessSize, sizeof(guessSize), 0);
+            if (n != guessSize) {
+                perror("Read Error: Guess size not read properly");
+                exit(EXIT_FAILURE);
+            }
+
+            n = recv(sd, guess, guessSize, 0);
+            if (n != guessSize) {
+                perror("Read Error: Guess not read properly");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Opponent entered \"%s\" \n",guess);
+
+        }
+        else{
+            printf("Opponent lost the round!\n");
+        }
+    }
+}
+
 void play_game(int sd, char playerNum, uint8_t board_size, uint8_t turn_time) {
   char guessBuffer[MAXWORDSIZE+1];
   char board[board_size+1];
-  uint8_t player1Score;
-  uint8_t player2Score;
+  uint8_t player1Score = 0;
+  uint8_t player2Score = 0;
   uint8_t roundNum;
   ssize_t n;
   int i; //for loop
@@ -119,46 +181,50 @@ void play_game(int sd, char playerNum, uint8_t board_size, uint8_t turn_time) {
   memset(guessBuffer,0,sizeof(guessBuffer)); //this essentially adds the null  terminator to the board for us.
   memset(board,0,sizeof(board));
 
-  //while(1) {
-    //R.1
-    n = read(sd,&player1Score,sizeof(player1Score));
-    if(n != sizeof(player1Score)){
-      perror("Read Error: Player1 Score not read properly");
-      exit(EXIT_FAILURE);
-    }
+  while(player1Score < 3 && player2Score < 3) {
+      //R.1
+      n = read(sd, &player1Score, sizeof(player1Score));
+      if (n != sizeof(player1Score)) {
+          perror("Read Error: Player1 Score not read properly");
+          exit(EXIT_FAILURE);
+      }
 
-    n = read(sd,&player2Score,sizeof(player2Score));
-    if(n != sizeof(player2Score)){
-      perror("Read Error: Player2 Score not read properly");
-      exit(EXIT_FAILURE);
-    }
+      n = read(sd, &player2Score, sizeof(player2Score));
+      if (n != sizeof(player2Score)) {
+          perror("Read Error: Player2 Score not read properly");
+          exit(EXIT_FAILURE);
+      }
 
-    //R.2
-    n = read(sd,&roundNum,sizeof(roundNum));
-    if(n != sizeof(roundNum)){
-      perror("Read Error: Round number not read properly");
-      exit(EXIT_FAILURE);
-    }
+      //R.2
+      n = read(sd, &roundNum, sizeof(roundNum));
+      if (n != sizeof(roundNum)) {
+          perror("Read Error: Round number not read properly");
+          exit(EXIT_FAILURE);
+      }
 
-    //R.4
-    n = recv(sd,board,board_size,0);
-    if(n != board_size){
-      perror("Read Error: Board not read properly");
-      exit(EXIT_FAILURE);
-    }
+      //R.4
+      n = recv(sd, board, board_size, 0);
+      if (n != board_size) {
+          perror("Read Error: Board not read properly");
+          exit(EXIT_FAILURE);
+      }
 
-    printf("Round %d... \n",roundNum);
-    if(playerNum == '1') {
-      printf("Score is %d-%d\n",player1Score,player2Score);
-    } else {
-      printf("Score is %d-%d\n",player2Score,player1Score);
-    }
-    //R.4
-    printf("Board:");
-    for(i = 0; i < board_size; i++) {
-      printf(" %c", board[i]);
-    }
-    printf("\n");
+      printf("Round %d... \n", roundNum);
+      if (playerNum == '1') {
+          printf("Score is %d-%d\n", player1Score, player2Score);
+      } else {
+          printf("Score is %d-%d\n", player2Score, player1Score);
+      }
+      //R.4
+      printf("Board:");
+      for (i = 0; i < board_size; i++) {
+          printf(" %c", board[i]);
+      }
+      printf("\n");
+
+      turn_handler(sd);
+
+  }
 
 /*
   //read the number of guesses
