b54b2eac08653bef943170d5cfa129946e9f8760
Zachary David Richardson <richarz@wwu.edu>
2018-11-15 17:59:18 -0800

    need: better send and rec, better disconnect errors, fix single char guess error

--- trie.h
+++ trie.h
@@ -1,25 +1,23 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <stdbool.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-// Alphabet size (# of symbols) 
-#define ALPHABET_SIZE (26) 
-  
-// Converts key current character into index 
-// use only 'a' through 'z' and lower case 
-#define CHAR_TO_INDEX(c) ((int)c - (int)'a') 
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (26)
 
-struct TrieNode 
-{ 
-    struct TrieNode *children[ALPHABET_SIZE]; 
-  
-    // isEndOfWord is true if the node represents 
-    // end of a word 
-    bool isEndOfWord; 
-}; 
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+
+struct TrieNode { 
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isEndOfWord is true if the node represents
+    // end of a word
+    bool isEndOfWord;
+};
 
 struct TrieNode *getNode(void);
 void insert(struct TrieNode *root, const char *key);
 bool search(struct TrieNode *root, const char *key);
-
